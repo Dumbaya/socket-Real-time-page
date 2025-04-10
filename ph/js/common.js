cc9c40f34e514c89de1b56aa63b212e0ef00331f
wkrns9 <wkrns9@naver.com>
2025-04-10 09:29:34 +0900

    로그인, 회원가입 구현 추가 push

--- ph/js/common.js
+++ ph/js/common.js
@@ -1,3 +1,10 @@
 function navigate(arg){
 	window.location.href=arg;
 }
+
+function setSession(key, value){
+	sessionStorage.setItem(key, value);
+}
+function getSession(key){
+	return sessionStorage.getItem(key);
+}
